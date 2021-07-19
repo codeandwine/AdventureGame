ad3dff9dbcaa30ed8cd32032ecae19163821b305
codeandwine <codeandwine@gmail.com>
2021-07-19 16:28:26 +0800

    Intro Message

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ import random
 def print_sleep(message)
 	print(message)
 	time.sleep(1.5) #change this line for the pauses
-
+def intro():
+	print_sleep("You find you are in the Jungles of Belize, filled with Mayan witchcraft and supernatural creatures.")
+	print_sleep("Rumor has it that an evil sprit is still roaming the mountains.")
+	print_sleep("In front of you is a Mayan Temple.")
+	print_sleep("Beside the temple there is a roaring river.")
+	print_sleep("In your hand you hold a hatchet, though it's a bit blunt.")
