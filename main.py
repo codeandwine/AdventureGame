b334104201f95141a9e6da7fb5e130311acff9cb
codeandwine <codeandwine@gmail.com>
2021-07-26 15:14:38 +0800

    file editted to fit pycodestyle

--- main.py
+++ main.py
@@ -5,100 +5,111 @@ start = time.time()
 enemies = ['Death Spirit', 'Duende', 'Zipacná', 'Xibalba', 'Buluc Chabtan']
 enemy = random.choice(enemies)
 
+
 def print_sleep(message):
-	print(message)
-	time.sleep(1.5) #change this line for the pauses
+    print(message)
+    time.sleep(1.5)
+    # change this line for the pauses
+
 
 def intro(weapon):
-	print_sleep("You find you are in the Jungles of Belize, filled with Mayan "
-				"witchcraft and supernatural creatures.")
-	print_sleep("Rumor has it that an evil sprit is still roaming the mountains.")
-	print_sleep("In front of you is a Mayan Temple.")
-	print_sleep("Beside the temple there is a roaring river.")
-	print_sleep("In your hand you hold a hatchet, though it's a bit blunt.\n")
-	options(weapon)
+    print_sleep("You find you are in the Jungles of Belize, filled with Mayan "
+                "witchcraft and supernatural creatures.")
+    print_sleep("Rumor has it that an evil sprit is still roaming "
+                "the mountains.")
+    print_sleep("In front of you is a Mayan Temple.")
+    print_sleep("Beside the temple there is a roaring river.")
+    print_sleep("In your hand you hold a hatchet, though it's a bit blunt.\n")
+    options(weapon)
+
 
 def options(weapon):
-	start = '0'
-	print_sleep("Enter 1 to knock on the door of the Mayan Temple.")
-	print_sleep("Enter 2 to drink some water from the river.")
+    start = '0'
+    print_sleep("Enter 1 to knock on the door of the Mayan Temple.")
+    print_sleep("Enter 2 to drink some water from the river.")
 
-	while start not in ['1', '2']:
-		start = input("(Please enter the numbers 1 or 2.)\n")
+    while start not in ['1', '2']:
+        start = input("(Please enter the numbers 1 or 2.)\n")
 
-		if start == '1':
-			knock_on_door(weapon)
-		elif start == '2':
-			drink_water(weapon)
-
+        if start == '1':
+            knock_on_door(weapon)
+        elif start == '2':
+            drink_water(weapon)
 
 
 def knock_on_door(weapon):
-	first_step = '0'
-	print_sleep("You walk towards the weird door at the top of the Temple.")
-	print_sleep(f"You are about to reach the door when a grotesque looking "
-				f"{enemy} opens the door.")
-	print_sleep("Holy Molly! This is the evil spirit encarnate!")
-	print_sleep(f"The ugly {enemy} attacks you. You are quite frightened about this!")
-	print_sleep("What should you do?")
-	print_sleep(f"1. Use the {weapon} to pierce his heart.")
-	print_sleep("2. RUN AWAY!")
+    first_step = '0'
+    print_sleep("You walk towards the weird door at the top of the Temple.")
+    print_sleep(f"You are about to reach the door when a grotesque looking "
+                f"{enemy} opens the door.")
+    print_sleep("Holy Molly! This is the evil spirit encarnate!")
+    print_sleep(f"The ugly {enemy} attacks you. You are quite frightened "
+                "about this!")
+    print_sleep("What should you do?")
+    print_sleep(f"1. Use the {weapon} to pierce his heart.")
+    print_sleep("2. RUN AWAY!")
 
-	while first_step not in ['1', '2']:
-		first_step = input("(Please enter the numbers 1 or 2.)\n")
-		
-		if first_step == '1':
-			pierce_heart(weapon)
-		elif first_step == '2':
-			run_away(weapon)
+    while first_step not in ['1', '2']:
+        first_step = input("(Please enter the numbers 1 or 2.)\n")
+
+        if first_step == '1':
+            pierce_heart(weapon)
+        elif first_step == '2':
+            run_away(weapon)
+
 
 def drink_water(weapon):
-	if weapon == "Magical Sword":
-		print_sleep("You have been here before. You already found the Magical "
-					"sword. You drink some water.")
-	else:
-		print_sleep("You walk towards the river bank. ")
-		print_sleep("As you bend down to drink, you see something glistening "
-					"in the water. You have found a magical sword!")
-		print_sleep("You rid yourself of your hatchet and take the sword.")
-		weapon = "Magical Sword"
+    if weapon == "Magical Sword":
+        print_sleep("You have been here before. You already found the Magical "
+                    "sword. You drink some water.")
+    else:
+        print_sleep("You walk towards the river bank. ")
+        print_sleep("As you bend down to drink, you see something glistening "
+                    "in the water. You have found a magical sword!")
+        print_sleep("You rid yourself of your hatchet and take the sword.")
+        weapon = "Magical Sword"
 
-	print_sleep("You head back to the jungle.\n")
-	options(weapon)
+    print_sleep("You head back to the jungle.\n")
+    options(weapon)
+
 
 def run_away(weapon):
-	print_sleep("You run back to where you started in the Jungle of Belize!")
-	print_sleep(f"Luckily the {enemy} didn't follow you.\n")
-	options(weapon)
+    print_sleep("You run back to where you started in the Jungle of Belize!")
+    print_sleep(f"Luckily the {enemy} didn't follow you.\n")
+    options(weapon)
+
 
 def pierce_heart(weapon):
-	play_again = "x"
+    play_again = "x"
 
-	if weapon == "Magical Sword":
-		print_sleep(f"As the {enemy} makes a move at you, you take the {weapon} "
-					"and pierce his heart.")
-		print_sleep("The body collapses and the spirit "
-					"is thrown down into the underworld.")
-		print_sleep("You have rid the Mayan Temple of the evil spirit!")
-		print_sleep("You have won!")
-	elif weapon == "Hatchet":
-		print_sleep(f"You do your best to aim at {enemy}'s heart.")
-		print_sleep("The human form of it is too swift and he evades your strike.")
-		print_sleep("He possesses your body and steals your soul!")
-		print_sleep("You have been defeated! :(")
+    if weapon == "Magical Sword":
+        print_sleep(f"As the {enemy} makes a move at you, you take the "
+                    f"{weapon} and pierce his heart.")
+        print_sleep("The body collapses and the spirit "
+                    "is thrown down into the underworld.")
+        print_sleep("You have rid the Mayan Temple of the evil spirit!")
+        print_sleep("You have won!")
+    elif weapon == "Hatchet":
+        print_sleep(f"You do your best to aim at {enemy}'s heart.")
+        print_sleep("The human form of it is too swift and he evades your "
+                    "strike.")
+        print_sleep("He possesses your body and steals your soul!")
+        print_sleep("You have been defeated! :(")
 
-	while play_again not in ['y', 'n']:
-		play_again = input("Would you like to play again? (y/n)\n").lower()
+    while play_again not in ['y', 'n']:
+        play_again = input("Would you like to play again? (y/n)\n").lower()
 
-		if play_again == 'y':
-			print_sleep("Fantastic! Let's take you back...\n")
-			weapon = "Hatchet"
-			intro(weapon)
-		elif play_again == 'n':
-			bye()
+        if play_again == 'y':
+            print_sleep("Fantastic! Let's take you back...\n")
+            weapon = "Hatchet"
+            intro(weapon)
+        elif play_again == 'n':
+            bye()
+
 
 def bye():
-	print_sleep("Thanks for playing! See you next time. :)")
+    print_sleep("Thanks for playing! See you next time. :)")
+
 
 weapon = "Hatchet"
 intro(weapon)
