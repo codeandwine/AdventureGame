569de3bcf224df072d8bb5e804a8133d0860fb9b
codeandwine <codeandwine@gmail.com>
2021-07-26 09:22:55 +0800

    First Option added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 #I'll put some stuff here in a bit
 
-def print_sleep(message)
+def print_sleep(message):
 	print(message)
 	time.sleep(1.5) #change this line for the pauses
 def intro():
@@ -15,3 +15,20 @@ def intro():
 	print_sleep("Enter 1 to knock on the door of the Mayan Temple.")
 	print_sleep("Enter 2 to peep into the Mayan Temple.")
 	print_sleep("(Please enter the numbers 1 or 2.)")
+
+def knock_on_door():
+	first_step = 0
+	print_sleep("You walk towards the weird door at the top of the Temple.")
+	print_sleep("You are about to reach the door when a grotesque looking Shaman opens the door.")
+	print_sleep("Holy Molly! This is the evil spirit encarnate!")
+	print_sleep("The ugly shaman attacks you. You are quite unprepared for this!")
+	print_sleep("What should you do?")
+	print_sleep("1. Use the hatchet to pierce his heart.")
+	print_sleep("2. RUN AWAY!")
+
+	first_step = input()
+
+	if first_step == 1:
+		pierce_heart()
+	elif first_step == 2:
+		run_away()
