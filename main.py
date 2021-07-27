fbd0259dc354a805da4b0727fb65516b95698ccb
codeandwine <codeandwine@gmail.com>
2021-07-27 09:12:23 +0800

    Indentation fixes to adhere pycodestyle

--- main.py
+++ main.py
@@ -13,19 +13,17 @@ def print_sleep(message, seconds):
 def intro(weapon, enemy):
     print_sleep("You find you are in the Jungle of Belize, filled with Mayan "
                 "witchcraft and supernatural creatures.", 1)
-    print_sleep("Rumor has it that an evil sprit is still roaming "
-                "the mountains.", 1)
+    print_sleep('Rumor has it that an evil sprit is still roaming the '
+                'mountains.', 1.5)
     print_sleep("In front of you is a Mayan Temple.", 1)
     print_sleep("Beside the temple there is a roaring river.", 1)
-    print_sleep("In your hand you hold a hatchet, though it's a bit blunt.\n", 2)
+    print_sleep("In your hand you hold a hatchet, though it's a bit blunt."
+                "\n", 2)
     options(weapon, enemy)
 
 
-def options(weapon, enemy):
+def first_choice(weapon, enemy):
     start = '0'
-    print_sleep("Enter 1 to knock on the door of the Mayan Temple.", 1)
-    print_sleep("Enter 2 to drink some water from the river.", 1)
-
     while start not in ['1', '2']:
         start = input("(Please enter the numbers 1 or 2.)\n")
 
@@ -35,8 +33,40 @@ def options(weapon, enemy):
             drink_water(weapon, enemy)
 
 
+def second_choice(weapon, enemy):
+    sec_step = '0'
+    while first_step not in ['1', '2']:
+        sec_step = input("(Please enter the numbers 1 or 2.)\n")
+
+        if sec_step == '1':
+            pierce_heart(weapon, enemy)
+        elif sec_step == '2':
+            run_away(weapon, enemy)
+
+
+def final_choice(weapon, enemy):
+    while play_again not in ['y', 'n']:
+        play_again = input("Would you like to play again? (y/n)\n").lower()
+
+        if play_again == 'y':
+            print_sleep("Fantastic! Let's take you back...\n", 2)
+            enemies = ['Death Spirit', 'Duende', 'Zipacná', 'Xibalba',
+                       'Buluc Chabtan']
+            enemy = random.choice(enemies)
+            weapon = "Hatchet"
+
+            intro(weapon, enemy)
+        elif play_again == 'n':
+            bye()
+
+
+def options(weapon, enemy):
+    print_sleep("Enter 1 to knock on the door of the Mayan Temple.", 1)
+    print_sleep("Enter 2 to drink some water from the river.", 1)
+    first_choice(weapon, enemy)
+
+
 def knock_on_door(weapon, enemy):
-    first_step = '0'
     print_sleep("You walk towards the weird door at the top of the Temple.", 1)
     print_sleep(f"You are about to reach the door when a grotesque looking "
                 f"{enemy} opens the door.", 2)
@@ -46,14 +76,7 @@ def knock_on_door(weapon, enemy):
     print_sleep("What should you do?", 1)
     print_sleep(f"1. Use the {weapon} to pierce his heart.", 1)
     print_sleep("2. RUN AWAY!", 1)
-
-    while first_step not in ['1', '2']:
-        first_step = input("(Please enter the numbers 1 or 2.)\n")
-
-        if first_step == '1':
-            pierce_heart(weapon, enemy)
-        elif first_step == '2':
-            run_away(weapon, enemy)
+    second_choice(weapon, enemy)
 
 
 def drink_water(weapon, enemy):
@@ -64,7 +87,7 @@ def drink_water(weapon, enemy):
         print_sleep("You walk towards the river bank. ", 1.5)
         print_sleep("As you bend down to drink, you see something glistening "
                     "in the water. You have found a magical sword!", 1.5)
-        print_sleep("You rid yourself of your hatchet and take the sword.", 1.5)
+        print_sleep("You rid yourself of your hatchet and take the sword.", 2)
         weapon = "Magical Sword"
 
     print_sleep("You head back to the jungle.\n", 2)
@@ -72,7 +95,8 @@ def drink_water(weapon, enemy):
 
 
 def run_away(weapon, enemy):
-    print_sleep("You run back to where you started in the Jungle of Belize!", 2)
+    print_sleep("You run back to where you started in the Jungle of "
+                "Belize!", 2)
     print_sleep(f"Luckily the {enemy} didn't follow you.\n", 1)
     options(weapon, enemy)
 
@@ -94,19 +118,12 @@ def pierce_heart(weapon, enemy):
         print_sleep("He possesses your body and steals your soul!", 1)
         print_sleep("You have been defeated! :(", 1.5)
 
-    while play_again not in ['y', 'n']:
-        play_again = input("Would you like to play again? (y/n)\n").lower()
-
-        if play_again == 'y':
-            print_sleep("Fantastic! Let's take you back...\n", 2)
-            weapon = "Hatchet"
-            intro(weapon, enemy)
-        elif play_again == 'n':
-            bye()
+    final_choice(weapon, enemy)
 
 
 def bye():
     print_sleep("Thanks for playing! See you next time. :)", 1)
+    exit(0)
 
 
 enemies = ['Death Spirit', 'Duende', 'Zipacná', 'Xibalba', 'Buluc Chabtan']
